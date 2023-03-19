9ee53b905fbccc37112252b936cf67d7d035c42d
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-03-19 01:33:58 -0400

    Add some style

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,14 @@ const App: React.FC = () => {
   }
 
   return (
-    <div>
+    <div className='grid grid-cols-3'>
       {data && data.map(post => (
-        <div key={post.id}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
+        <div key={post.id} className='border shadow-md my-4 mx-10 p-4 rounded-lg'>
+          <div className='flex items-center'>
+            <img src={`https://avatars.dicebear.com/api/adventurer/${post.id}.svg`} alt='avatar' className='w-10 h-10 rounded-full' />
+            <h3 className='font-bold uppercase'>{post.title}</h3>
+          </div>
+          <p className=''>{post.body}</p>
         </div>
       ))}
     </div>
